63ea0b558dc87eaa9030a3eda09164dbbe23bbae
Sami Nurmivaara <sami.nurmivaara@helsinki.fi>
2019-02-09 12:44:32 +0200

    Exercise 3.7 and 3.8. done, therefore completing part 3.a

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require('express')
 const app = express()
 const bodyParser = require ('body-parser')
+const morgan = require('morgan')
 
 app.use(bodyParser.json())
+
+morgan.token('body', function getBody (req) {
+  return JSON.stringify(req.body)
+})
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 let persons = [
   {
